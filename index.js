55ca9f476754204364cfae8943bee0511f445147
j2131 <166688074+j2131@users.noreply.github.com>
2024-06-06 07:08:54 +0000

    image

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path');
+
+app.use(loggerMiddleware);
+
+// 静的ファイルの提供とキャッシュの有効化
+app.use(express.static('public', { maxAge: 86400000 }));
+
+// test-imageリクエスト
+app.get('/test-image', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/images/test.png'));
+});
+
+
 app.get('/', (req, res) => {
   res.send({
     msg:'GET request'
